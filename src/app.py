307f9b895463374be6f46c91c13687c6b4ebdba7
Deyruby <128061593+Deyruby@users.noreply.github.com>
2024-02-29 19:32:30 +0000

    tarea terminada

--- src/app.py
+++ src/app.py
@@ -26,7 +26,42 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+    members = jackson_family.get_all_members()
+    return jsonify(members), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    return jsonify(member), 200
+
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    if jackson_family.delete_member(member_id):
+        members = jackson_family.get_all_members()
+        return jsonify({"done": True, "count": len(members)}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+    jackson_family.add_member(member)
+    return jsonify(''), 200
+
+
+
+
+
+
+
+
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
